b444a276de60e1f4bb18e9f2aa43b03770b8a306
Shreyas <shreyas@gmail.com>
2012-11-30 13:19:43 -0600

    more py tests

---
+++ t/cleanup.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+# This is a simple cleanup script to be 
+# called after testing to avoid leaving a mess 
+# - Shreyas Cholia 11/30/2012
+use Bio::KBase::workspaceService::Impl;
+my $impl = Bio::KBase::workspaceService::Impl->new();
+#Deleting test objects
+$impl->_clearAllWorkspaces();
+$impl->_clearAllWorkspaceObjects();
+$impl->_clearAllWorkspaceUsers();
+$impl->_clearAllWorkspaceDataObjects();
--- t/testWorkspaceService.py
+++ t/testWorkspaceService.py
@@ -2,6 +2,7 @@ import unittest
 from biokbase.auth.auth_token import get_token
 from biokbase.workspaceService.Client import workspaceService
 from datetime import datetime
+import os
 
 
 class TestWorkspaces(unittest.TestCase):
@@ -14,15 +15,17 @@ class TestWorkspaces(unittest.TestCase):
 
     def setUp(self):
         self.impl = workspaceService('http://localhost:7058')
-
+        # FIXME: Right now you can't delete so we'll create a new one each time.
+        self.ws_name = "testWS_%s" % datetime.utcnow().strftime('%s')
+        self.conf = {"workspace": self.ws_name,"default_permission": "a", "auth": self.__class__.token }
+        self.ws_meta = self.impl.create_workspace(self.conf)
     
     def testCreate(self):
         impl = self.impl
-        # FIXME: Right now you can't delete so we'll create a new one each time.
-        ws_name = "testWS_%s" % datetime.utcnow().strftime('%s')
+        ws_name = self.ws_name
+        conf = self.conf
+        ws_meta = self.ws_meta
 
-        conf = {"workspace": ws_name,"default_permission": "a", "auth": self.__class__.token }
-        ws_meta = impl.create_workspace(conf)
         self.assertEquals(ws_meta[0], ws_name)
         self.assertEquals(ws_meta[1], 'kbasetest')
         self.assertEquals(ws_meta[3], 0)
@@ -31,6 +34,61 @@ class TestWorkspaces(unittest.TestCase):
 
 
         impl.delete_workspace({"workspace": ws_name, "auth": self.__class__.token})
+
+    def testClone(self):
+        impl = self.impl
+        ws_name = self.ws_name
+        conf = self.conf
+        ws_meta = self.ws_meta
+
+        clone_ws_name = "clone_%s" % ws_name
+
+        clone = impl.clone_workspace({
+            "new_workspace": clone_ws_name,
+            "current_workspace": ws_name,
+            "default_permission": "n",
+            "auth": self.__class__.token 
+        })
+
+        self.assertEquals(clone[0], clone_ws_name)
+
+        impl.delete_workspace({"workspace": ws_name, "auth": self.__class__.token})
+        impl.delete_workspace({"workspace": clone_ws_name, "auth": self.__class__.token})
+
+
+    def testDelete(self):
+        impl = self.impl
+        ws_name = self.ws_name
+        conf = self.conf
+        ws_meta = self.ws_meta
+
+        impl.delete_workspace({"workspace": ws_name, "auth": self.__class__.token})
+
+        self.assert_(True)
+
+    def testListWorkspaces(self):
+        impl = self.impl
+        ws_name = self.ws_name
+        conf = self.conf
+        ws_meta = self.ws_meta
+
+        ws_name2 = "testWS_%s" % datetime.utcnow().strftime('%s')
+        conf2 = {"workspace": ws_name2,"default_permission": "a", "auth": self.__class__.token }
+        ws_meta2 = self.impl.create_workspace(conf2)
+
+        ws_list = impl.listWorkspaces({ "auth": self.__class__.token })
+
+        ws_names = [ w[0][0] for w in ws_list ]
+
+        self.assertIn(ws_name, ws_names)
+        self.assertIn(ws_name2, ws_names)
+
+
+    @classmethod
+    def tearDownClass(self):
+        test_dir = os.path.dirname(__file__)
+        cleanup_file = os.path.join(test_dir, 'cleanup.pl')
+        subprocess.call([cleanup_file])
 
 
 
