20511dcba449330326d79a0613a1e85986f6ab76
Shreyas <shreyas@gmail.com>
2012-11-30 01:11:36 -0600

    typo

--- t/testWorkspaceService.py
+++ t/testWorkspaceService.py
@@ -15,16 +15,16 @@ class TestWorkspaces(unittest.TestCase):
         impl = workspaceService('http://localhost:7058')
 
         # FIXME: Right now you can't delete so we'll create a new one each time.
-        # FIXME: The following is too long "testworkspace_1354258955.274313". Document the limits
+        # FIXME: The following is too long "testworkspace_1354258955.274313". Document the limits.
         ws_name = "testWS_%s" % datetime.utcnow().strftime('%s')
 
         conf = {"workspace": ws_name,"default_permission": "a", "auth": self.__class__.token }
         ws_meta = impl.create_workspace(conf)
-        self.assertEquals(wsmeta[0], ws_name)
-        self.assertEquals(wsmeta[1], 'kbasetest')
-        self.assertEquals(wsmeta[3], 0)
-        self.assertEquals(wsmeta[4], 'a')
-        self.assertEquals(wsmeta[5], 'a')
+        self.assertEquals(ws_meta[0], ws_name)
+        self.assertEquals(ws_meta[1], 'kbasetest')
+        self.assertEquals(ws_meta[3], 0)
+        self.assertEquals(ws_meta[4], 'a')
+        self.assertEquals(ws_meta[5], 'a')
 
 
         impl.delete_workspace({"workspace": ws_name, "auth": self.__class__.token})
