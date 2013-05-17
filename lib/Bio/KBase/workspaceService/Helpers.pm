e0106a1911f961f3b558cf89702dcca57cc0934b
Christopher Henry <chenry@mcs.anl.gov>
2013-05-17 11:54:36 -0500

    Corrections to command script and correction to model saving.

--- lib/Bio/KBase/workspaceService/Helpers.pm
+++ lib/Bio/KBase/workspaceService/Helpers.pm
@@ -4,7 +4,7 @@ use warnings;
 use Bio::KBase::workspaceService::Client;
 use Exporter;
 use parent qw(Exporter);
-our @EXPORT_OK = qw(printJobData auth get_ws_client workspace workspaceURL parseObjectMeta parseWorkspaceMeta printObjectMeta printWorkspaceMeta);
+our @EXPORT_OK = qw(loadTableFile printJobData auth get_ws_client workspace workspaceURL parseObjectMeta parseWorkspaceMeta printObjectMeta printWorkspaceMeta);
 our $defaultURL = "http://kbase.us/services/workspace/";
 
 my $CurrentWorkspace;
@@ -221,6 +221,28 @@ sub printJobData {
     if (defined($job->{jobdata}->{error})) {
     	print "Error: ".$job->{jobdata}->{error}."\n";
     }    
+}
+
+sub loadTableFile {
+	my ($filename) = @_;
+	if (!-e $filename) {
+		print "Could not open table file ".$filename."!\n";
+		exit();
+	}
+	open(my $fh, "<", $filename) || return;
+	my $headingline = <$fh>;
+	my $tbl;
+	chomp($headingline);
+	my $headings = [split(/\t/,$headingline)];
+	for (my $i=0; $i < @{$headings}; $i++) {
+		$tbl->{headings}->{$headings->[$i]} = $i;
+	}
+	while (my $line = <$fh>) {
+		chomp($line);
+		push(@{$tbl->{data}},[split(/\t/,$line)]);
+	}
+	close($fh);
+	return $tbl;
 }
 
 1;
